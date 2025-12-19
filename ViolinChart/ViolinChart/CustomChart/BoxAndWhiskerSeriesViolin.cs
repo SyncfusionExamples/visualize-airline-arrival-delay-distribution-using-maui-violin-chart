533307868c0bae1e4587c12ef82d8a1f2af333d1
natrayansf <natrayan.ramalingam@syncfusion.com>
2025-12-19 19:40:54 +0530

    Added toolkit project instead es

--- ViolinChart/ViolinChart/CustomChart/BoxAndWhiskerSeriesViolin.cs
+++ ViolinChart/ViolinChart/CustomChart/BoxAndWhiskerSeriesViolin.cs
@@ -1,4 +1,4 @@
-﻿using Syncfusion.Maui.Charts;
+﻿using Syncfusion.Maui.Toolkit.Charts;
 using System;
 using System.Collections.Generic;
 using System.Text;
--- ViolinChart/ViolinChart/MauiProgram.cs
+++ ViolinChart/ViolinChart/MauiProgram.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
-using Syncfusion.Maui.Core.Hosting;
+using Syncfusion.Maui.Toolkit.Hosting;
 
 namespace ViolinChart
 {
@@ -10,7 +10,7 @@ namespace ViolinChart
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
-                .ConfigureSyncfusionCore()
+                .ConfigureSyncfusionToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
